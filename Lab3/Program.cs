3fed7d81068df21e86815272c8b22b584a4a1ea2
Kendall <kendallallmand@gmail.com>
2020-04-21 16:46:42 -0400

    the fourth

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Principal;
 
 namespace Lab3
 {
@@ -10,61 +11,58 @@ namespace Lab3
             Console.WriteLine("Welcome to Grand Circus Digit Validator!");
             Console.WriteLine("Please enter your name: ");
             string name = Console.ReadLine();
-            int validcheck = 1;
-            
-            // Entry Validation
-            Console.WriteLine("Enter a number between 1 and 100");
-            string input = Console.ReadLine();
-            int i = 0;
+            int cont = 1;
 
-            while (validcheck == 1)
+            while (cont == 1)
             {
-                if (input.All(char.IsNumber)){
-                    validcheck = 0;
-                }
-                else
-                {
-                    validcheck = 2;
-                }
+                // Entry Validation
+                Console.WriteLine("Enter a number between 1 and 100");
+                string input = Console.ReadLine();
+                int i = 0;
+                int validcheck = 1;
+                int validcheck2 = 1;
 
-                if (validcheck != 2)
+                while (validcheck == 1)
                 {
-                    i = int.Parse(input);
-                    if (input.All(char.IsDigit))
+                    if (input.All(char.IsNumber))
                     {
                         validcheck = 0;
-                    }
-                    if (i >= 1 && i <= 100)
-                    {
-                        validcheck = 0;
-                    }
                     }
                     else
                     {
-                    Console.WriteLine("invalid entry, please try again");
-                    Console.WriteLine("Enter a number between 1 and 100");
-                    input = Console.ReadLine();
-                    i = int.Parse(input);
+                        validcheck = 2;
                     }
 
-           //OddEven Results
+                    if (validcheck != 2)
+                    {
+                        i = int.Parse(input);
+    
+                    }
+                    if (i >= 1 && i <= 100)
+                    {
+                        validcheck2 = 0;
+                    }
+
+                    if (validcheck == 2 || validcheck2 == 2)
+                        {
+                        validcheck = 1;
+                        validcheck2 = 1;
+                        Console.WriteLine("invalid entry, please try again");
+                        Console.WriteLine("Enter a number between 1 and 100");
+                        input = Console.ReadLine();
+                    }
+                    
+                }
+
+                //OddEven Variable
                 int oddeven = 0;
                 if (i % 2 == 0)
-                {
-                    oddeven = 2;
-
-                }
-                else
-                {
-                    oddeven = 1;
-                    Console.WriteLine($"{name}, the number you entered {i} is odd");
-                }
-
+                { oddeven = 2; }
+                else { oddeven = 1; }
                 if (oddeven == 2 && i >= 2 && i <= 25)
                 {
                     Console.WriteLine($"{name}, the number you entered {i} is even and less than 25!");
                 }
-
                 if (oddeven == 2 && i >= 26 && i <= 60)
                 {
                     Console.WriteLine($"{name}, the number you entered {i} is even and more than 26!");
@@ -77,46 +75,44 @@ namespace Lab3
                 {
                     Console.WriteLine($"{name}, the number you entered {i} is more than 60 and an odd number!");
                 }
-
-            //Continue?.. entry
-                Console.WriteLine("Would you like to continue? (y/n) ");
-                string proceed = Console.ReadLine();
-                Console.WriteLine();
-
-            //Continue variable
-                if (proceed.ToLower() == "y")
+                else if (oddeven == 1)
                 {
-                    Console.WriteLine("Enter a number between 1 and 100");
-                     input = Console.ReadLine();
-                     i = int.Parse(input);
-                    validcheck = 1;
+                    Console.WriteLine($"{name}, the number you entered {i} is odd");
                 }
-                else
-                {
-                    if (proceed.ToLower() == "yes")
-                    {
-                        proceed = "y";
-                        Console.WriteLine("Enter a number between 1 and 100");
-                        input = Console.ReadLine();
-                        i = int.Parse(input);
-                        validcheck = 1;
-                    }
 
+                int cont2 = 1;
+                while (cont2 == 1)
+                {
+                    //Continue?.. entry
+                    Console.WriteLine("Would you like to continue? (y/n) ");
+                    string proceed = Console.ReadLine();
+                    Console.WriteLine();
+
+                    //Continue variable
+                    if (proceed.ToLower() == "y")
+                    { 
+                        cont2 = 2; 
+                    }
                     if (proceed.ToLower() == "n")
                     {
+                        cont = 0;
+                        cont2 = 1;
                         break;
                     }
-
                     if (proceed.ToLower() == "no")
                     {
+                        cont = 0;
+                        cont2 = 1;
                         break;
-
                     }
-                    else
+                    if (proceed.ToLower() == "yes")
+                    { 
+                        cont2 = 2; 
+                    }
+                    if (cont2 == 1)
                     {
                         Console.WriteLine("invalid entry! please try again.");
-                        Console.WriteLine("Would you like to continue?(y/n)");
-                        proceed = Console.ReadLine();
+                        cont2 = 1;
                         Console.WriteLine();
                     }
                 }
@@ -124,5 +120,7 @@ namespace Lab3
         }
     }
 }
+
+
 
 
